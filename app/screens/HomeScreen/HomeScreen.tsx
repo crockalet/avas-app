2575bdd547d8d79c08e4e5895b738bf2e452694e
crockalet <isse_1997@hotmail.com>
2025-05-20 01:47:17 +0500

    feat: header

--- app/screens/HomeScreen/HomeScreen.tsx
+++ app/screens/HomeScreen/HomeScreen.tsx
@@ -1,10 +1,28 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { HeroIcon } from "../../components/HeroIcon";
 
 export default function HomeScreen() {
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>Home Screen</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={{ flexDirection: "row", alignItems: "flex-end", gap: 12 }}>
+        <HeroIcon name="truck" size={38} color="#000" />
+        <Text style={styles.title}>Avas App</Text>
+      </View>
+    </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    paddingHorizontal: 16,
+    paddingTop: 24,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "900",
+  },
+});
